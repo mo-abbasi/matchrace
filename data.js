979294480a519f0c022203fda12b9f5b9f03a03a
Mo Abbasi <moa@heka-dental.dk>
2024-09-03 13:30:18 +0200

    show live results -2

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 // data.js
-const eventData = {
-    "eventName": "Sample Event",
-    "teams": ["Team A", "Team B", "Team C"],
-    "completedRaces": [
-        {"winner": "Team A", "loser": "Team B"},
-        {"winner": "Team C", "loser": "Team A"}
-    ]
-};
+const eventData = [
+    {
+        "eventName": "Womens World Match Racing Tour - Denmark - Grade 1 - 2024",
+        "teams": ["Team Out of the Box", "Bow Down Racing", "Amphitrite Racing", "Those Seagulls", "Shark Caviar", "WOW Racing", "Team Louise", "Ulrikkeholm", "IRL", "Beyond Racing Team"],
+        "completedRaces": [
+            
+        ]
+    },
+];
+
