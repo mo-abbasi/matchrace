9547464ce4ac3db51d79cb37659644e37a88e1a5
Mo Abbasi <moa@heka-dental.dk>
2024-09-03 12:56:31 +0200

    Show live results from a file

--- data.js
+++ data.js
@@ -1,8 +1,9 @@
-{
+// data.js
+const eventData = {
     "eventName": "Sample Event",
     "teams": ["Team A", "Team B", "Team C"],
     "completedRaces": [
         {"winner": "Team A", "loser": "Team B"},
         {"winner": "Team C", "loser": "Team A"}
     ]
-}
+};
