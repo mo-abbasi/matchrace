308ff68f3ce4f8a08f62386b8e3d14ea97a98b88
Mo Abbasi <moa@heka-dental.dk>
2024-09-03 13:43:08 +0200

    fixed event name

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 // data.js
 const eventData = [
     {
-        "eventName": "Womens World Match Racing Tour - Denmark - Grade 1 - 2024",
+        "eventName": "Women's World Match Racing Tour - Denmark - Grade 1 - 2024",
         "teams": ["Team Out of the Box", "Bow Down Racing", "Amphitrite Racing", "Those Seagulls", "Shark Caviar", "WOW Racing", "Team Louise", "Ulrikkeholm", "IRL", "Beyond Racing Team"],
         "completedRaces": [
             
