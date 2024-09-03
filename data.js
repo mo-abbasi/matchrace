a33a9d13c4fe54e50aca67607d9afa8cba7dec7a
mo-abbasi <mo82abbasi@gmail.com>
2024-09-03 14:00:25 +0200

    Update data.js test

--- data.js
+++ data.js
@@ -4,6 +4,7 @@ const eventData = [
         "eventName": "Women's World Match Racing Tour - Denmark - Grade 1 - 2024",
         "teams": ["Team Out of the Box", "Bow Down Racing", "Amphitrite Racing", "Those Seagulls", "Shark Caviar", "WOW Racing", "Team Louise", "Ulrikkeholm", "IRL", "Beyond Racing Team"],
         "completedRaces": [
+            {"winner": "Team Out of the Box", "loser": "Bow Down Racing"},
             
         ]
     },
